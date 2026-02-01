c289d4dfb4f5d004e7e901bc98860b3e50e84896
Tim Healy <thealy5011@gmail.com>
2026-01-31 22:54:18 -0800

    starting to understand cooley tukey

--- cooley_tukey.py
+++ cooley_tukey.py
@@ -2,3 +2,8 @@ import numpy as np
 
 # Implementation of FFT using Cooley-Tukey algorithm
 
+# Okay so to compute the fourier coefficnents we are taking the dot product of the signal with different frequency basis vectors (exponentials)
+# We can reorder the samples by the bit-reversal permutation
+# The point of this is is that these samples will be dotted with essentially the same basis vectors (sign chance)?
+# The reason for this is due to the periodicity of the complex exponentials (basis vectors)
+# I'm not totally sure how separating the samples by even and odd indicies corresponds to this periodicity
